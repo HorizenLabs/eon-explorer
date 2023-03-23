92f4ebfe62eae842c4959dd4c033c0834ed42433
Shaun Gould <shaun@horizenlabs.io>
2023-03-23 16:12:40 -0400

    feat: migrate to new forward_transfers table (#130)

--- apps/explorer/priv/repo/migrations/20230323193347_recreate_forward_transfers.exs
+++ apps/explorer/priv/repo/migrations/20230323193347_recreate_forward_transfers.exs
@@ -1,13 +1,13 @@
-defmodule Explorer.Repo.Migrations.CreateForwardTransfers do
+defmodule Explorer.Repo.Migrations.RecreateForwardTransfers do
   use Ecto.Migration
 
   def change do
+    drop_if_exists(table(:forward_transfers))
     create table(:forward_transfers, primary_key: true) do
       add(:to_address_hash, :string, null: false)
-      add(:value, :bigint, null: false)
+      add(:value, :numeric, precision: 100, null: false)
       add(:block_number, :bigint, null: false)
       timestamps(null: false, type: :utc_datetime_usec)
     end
-
   end
 end
