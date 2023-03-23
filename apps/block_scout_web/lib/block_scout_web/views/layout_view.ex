02700d3da00e25f43f632545c0f663251d885f03
Shaun Gould <shaun@horizenlabs.io>
2023-03-23 10:20:11 -0400

    feat: env var configure faucet and metadata (#125)

--- apps/block_scout_web/lib/block_scout_web/views/layout_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/layout_view.ex
@@ -5,6 +5,7 @@ defmodule BlockScoutWeb.LayoutView do
   alias Plug.Conn
   alias Poison.Parser
 
+  import BlockScoutWeb.AddressView, only: [from_address_hash: 1]
   import BlockScoutWeb.APIDocsView, only: [blockscout_url: 1]
 
   @default_other_networks [
@@ -61,6 +62,10 @@ defmodule BlockScoutWeb.LayoutView do
 
   def configured_social_media_services do
     SocialMedia.links()
+  end
+
+  def meta_image_url do
+    "#{blockscout_url(false)}#{Application.get_env(:block_scout_web, :meta_image_url)}"
   end
 
   def issue_link(conn) do
