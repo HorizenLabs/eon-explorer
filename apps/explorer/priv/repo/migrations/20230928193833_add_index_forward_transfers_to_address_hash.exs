c24bf00eddd08d638bad0fb484db052105224f32
voyenavion <sgould3@gmail.com>
2023-09-28 16:58:38 -0400

    feat: index on to_address for ft and fp tables

---
+++ apps/explorer/priv/repo/migrations/20230928193833_add_index_forward_transfers_to_address_hash.exs
@@ -0,0 +1,16 @@
+defmodule Explorer.Repo.Migrations.AddIndexForwardTransfersToAddressHash do
+  use Ecto.Migration
+  @disable_ddl_transaction true
+  @disable_migration_lock true
+
+  def change do
+     create_if_not_exists(
+      index(
+        :forward_transfers,
+        [:to_address_hash, "block_number DESC", "index DESC"],
+        name: "forward_transfers_to_address_hash_index",
+        concurrently: true
+      )
+    )
+  end
+end
