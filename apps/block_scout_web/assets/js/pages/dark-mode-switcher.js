7d54d87f105404c5b93a6c68e2b66a4e8b571fcd
davide <davidebanfi@horizenlabs.io>
2023-09-29 17:26:07 +0200

    TOOLS-2625 - add cookie duration

--- apps/block_scout_web/assets/js/pages/dark-mode-switcher.js
+++ apps/block_scout_web/assets/js/pages/dark-mode-switcher.js
@@ -4,9 +4,9 @@ const darkModeChangerEl = document.getElementsByClassName('dark-mode-changer')[0
 
 darkModeChangerEl && darkModeChangerEl.addEventListener('click', function () {
   if (Cookies.get('chakra-ui-color-mode') === 'dark') {
-    Cookies.set('chakra-ui-color-mode', 'light')
+    Cookies.set('chakra-ui-color-mode', 'light', {expires: 30})
   } else {
-    Cookies.set('chakra-ui-color-mode', 'dark')
+    Cookies.set('chakra-ui-color-mode', 'dark', {expires: 30})
   }
   document.location.reload()
 })
