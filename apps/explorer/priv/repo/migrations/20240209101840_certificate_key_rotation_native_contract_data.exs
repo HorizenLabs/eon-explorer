db3fd3145bf48c93afb3889e94129f09b778fa53
davide <davidebanfi@horizenlabs.io>
2024-02-16 13:04:34 +0100

    native contracts script fix for blue/green deployment support

--- apps/explorer/priv/repo/migrations/20240209101840_certificate_key_rotation_native_contract_data.exs
+++ apps/explorer/priv/repo/migrations/20240209101840_certificate_key_rotation_native_contract_data.exs
@@ -38,30 +38,52 @@ defmodule Explorer.Repo.Migrations.CertificateKeyRotationNativeContractData do
        END $$"
     )
 
-    execute("
-      INSERT INTO smart_contracts (
-        name,
-        compiler_version,
-        optimization,
-        contract_source_code,
-        abi,
-        address_hash,
-        inserted_at,
-        updated_at,
-        contract_code_md5)
-      VALUES (
-        'Certificate Key Rotation',
-        '-',
-        false,
-        '/',
-        '[{\"inputs\":[{\"internalType\":\"uint32\",\"name\":\"key_type\",\"type\":\"uint32\"},{\"internalType\":\"uint32\",\"name\":\"index\",\"type\":\"uint32\"},{\"internalType\":\"bytes32\",\"name\":\"newKey_1\",\"type\":\"bytes32\"},{\"internalType\":\"bytes1\",\"name\":\"newKey_2\",\"type\":\"bytes1\"},{\"internalType\":\"bytes32\",\"name\":\"signKeySig_1\",\"type\":\"bytes32\"},{\"internalType\":\"bytes32\",\"name\":\"signKeySig_2\",\"type\":\"bytes32\"},{\"internalType\":\"bytes32\",\"name\":\"masterKeySig_1\",\"type\":\"bytes32\"},{\"internalType\":\"bytes32\",\"name\":\"masterKeySig_2\",\"type\":\"bytes32\"},{\"internalType\":\"bytes32\",\"name\":\"newKeySig_1\",\"type\":\"bytes32\"},{\"internalType\":\"bytes32\",\"name\":\"newKeySig_2\",\"type\":\"bytes32\"}],\"name\":\"submitKeyRotation\",\"outputs\":[{\"internalType\":\"uint32\",\"name\":\"\",\"type\":\"uint32\"},{\"internalType\":\"uint32\",\"name\":\"\",\"type\":\"uint32\"},{\"internalType\":\"bytes32\",\"name\":\"\",\"type\":\"bytes32\"},{\"internalType\":\"bytes1\",\"name\":\"\",\"type\":\"bytes1\"},{\"internalType\":\"bytes32\",\"name\":\"\",\"type\":\"bytes32\"},{\"internalType\":\"bytes32\",\"name\":\"\",\"type\":\"bytes32\"},{\"internalType\":\"bytes32\",\"name\":\"\",\"type\":\"bytes32\"},{\"internalType\":\"bytes32\",\"name\":\"\",\"type\":\"bytes32\"}],\"stateMutability\":\"nonpayable\",\"type\":\"function\"}]',
-        E'\\\\x0000000000000000000044444444444444444444',
-        NOW(),
-        NOW(),
-        '6666cd76f96956469e7be39d750cc7d9'
-      );")
+    execute(
+      "DO $$
+        BEGIN
 
-    execute("INSERT INTO address_names(address_hash, name, \"primary\", inserted_at, updated_at) VALUES (E'\\\\x0000000000000000000044444444444444444444', 'Certificate Key Rotation', true, NOW(), NOW())")
+          -- Check if the record with the specified hash exists
+          IF NOT EXISTS (SELECT 1 FROM smart_contracts WHERE address_hash = E'\\\\x0000000000000000000044444444444444444444') THEN
+
+            INSERT INTO smart_contracts (
+              name,
+              compiler_version,
+              optimization,
+              contract_source_code,
+              abi,
+              address_hash,
+              inserted_at,
+              updated_at,
+              contract_code_md5)
+            VALUES (
+              'Certificate Key Rotation',
+              '-',
+              false,
+              '/',
+              '[{\"inputs\":[{\"internalType\":\"uint32\",\"name\":\"key_type\",\"type\":\"uint32\"},{\"internalType\":\"uint32\",\"name\":\"index\",\"type\":\"uint32\"},{\"internalType\":\"bytes32\",\"name\":\"newKey_1\",\"type\":\"bytes32\"},{\"internalType\":\"bytes1\",\"name\":\"newKey_2\",\"type\":\"bytes1\"},{\"internalType\":\"bytes32\",\"name\":\"signKeySig_1\",\"type\":\"bytes32\"},{\"internalType\":\"bytes32\",\"name\":\"signKeySig_2\",\"type\":\"bytes32\"},{\"internalType\":\"bytes32\",\"name\":\"masterKeySig_1\",\"type\":\"bytes32\"},{\"internalType\":\"bytes32\",\"name\":\"masterKeySig_2\",\"type\":\"bytes32\"},{\"internalType\":\"bytes32\",\"name\":\"newKeySig_1\",\"type\":\"bytes32\"},{\"internalType\":\"bytes32\",\"name\":\"newKeySig_2\",\"type\":\"bytes32\"}],\"name\":\"submitKeyRotation\",\"outputs\":[{\"internalType\":\"uint32\",\"name\":\"\",\"type\":\"uint32\"},{\"internalType\":\"uint32\",\"name\":\"\",\"type\":\"uint32\"},{\"internalType\":\"bytes32\",\"name\":\"\",\"type\":\"bytes32\"},{\"internalType\":\"bytes1\",\"name\":\"\",\"type\":\"bytes1\"},{\"internalType\":\"bytes32\",\"name\":\"\",\"type\":\"bytes32\"},{\"internalType\":\"bytes32\",\"name\":\"\",\"type\":\"bytes32\"},{\"internalType\":\"bytes32\",\"name\":\"\",\"type\":\"bytes32\"},{\"internalType\":\"bytes32\",\"name\":\"\",\"type\":\"bytes32\"}],\"stateMutability\":\"nonpayable\",\"type\":\"function\"}]',
+              E'\\\\x0000000000000000000044444444444444444444',
+              NOW(),
+              NOW(),
+              '6666cd76f96956469e7be39d750cc7d9'
+            );
+
+          END IF;
+        END $$"
+    )
+
+    execute(
+      "DO $$
+        BEGIN
+
+          -- Check if the record with the specified hash exists
+          IF NOT EXISTS (SELECT 1 FROM address_names WHERE address_hash = E'\\\\x0000000000000000000044444444444444444444') THEN
+
+          INSERT INTO address_names(address_hash, name, \"primary\", inserted_at, updated_at) VALUES (E'\\\\x0000000000000000000044444444444444444444', 'Certificate Key Rotation', true, NOW(), NOW());
+
+          END IF;
+        END $$"
+    )
+
     execute("INSERT INTO reserved_addresses(address_hash, name, is_contract, inserted_at) VALUES (E'\\\\x0000000000000000000044444444444444444444', 'certificate key rotation', true, NOW())")
 
   end
