5bd798677135c117ffa1c9c25d988fcbd623dc0d
davide <davidebanfi@horizenlabs.io>
2024-03-19 16:16:41 +0100

    WEB3-521 - block reward unit test for data not present in the table for existing blocks

--- apps/block_scout_web/test/block_scout_web/controllers/api/rpc/block_controller_test.exs
+++ apps/block_scout_web/test/block_scout_web/controllers/api/rpc/block_controller_test.exs
@@ -4,6 +4,8 @@ defmodule BlockScoutWeb.API.RPC.BlockControllerTest do
   alias Explorer.Chain.{Hash, Wei}
   alias BlockScoutWeb.Chain
 
+  require Logger
+
   describe "getblockreward" do
     test "with missing block number", %{conn: conn} do
       response =
@@ -40,6 +42,23 @@ defmodule BlockScoutWeb.API.RPC.BlockControllerTest do
         |> json_response(200)
 
       assert response["message"] =~ "Block does not exist"
+      assert response["status"] == "0"
+      assert Map.has_key?(response, "result")
+      refute response["result"]
+      schema = resolve_schema()
+      assert :ok = ExJsonSchema.Validator.validate(schema, response)
+    end
+
+    test "with a block that exist but with no record in block_reward table", %{conn: conn} do
+
+      block = insert(:block) # insert a block but no reward in block_reward table related to it
+
+      response =
+        conn
+        |> get("/api", %{"module" => "block", "action" => "getblockreward", "blockno" => block.number})
+        |> json_response(200)
+
+      assert response["message"] =~ "No block reward data related to this block"
       assert response["status"] == "0"
       assert Map.has_key?(response, "result")
       refute response["result"]
