67076a7db7efd9a470f4ac2262fcc300d408f809
davide <davidebanfi@horizenlabs.io>
2024-03-21 17:51:06 +0100

    WEB3-260 - view sintax fix

--- apps/block_scout_web/lib/block_scout_web/views/api/v2/fee_payment_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/api/v2/fee_payment_view.ex
@@ -3,7 +3,7 @@ defmodule BlockScoutWeb.API.V2.FeePaymentView do
   alias Explorer.Chain.Address
 
   def render("fee_payments.json", %{fee_payments: fee_payments, next_page_params: next_page_params}) do
-    %{"items" => Enum.map(fee_payments, &prepare_fee_payments(&1, nil)), "next_page_params" => next_page_params}
+    %{"items" => Enum.map(fee_payments, &prepare_fee_payment(&1, nil)), "next_page_params" => next_page_params}
   end
 
   def prepare_fee_payment(fee_payments, _conn) do
--- apps/block_scout_web/lib/block_scout_web/views/api/v2/forward_transfer_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/api/v2/forward_transfer_view.ex
@@ -3,7 +3,7 @@ defmodule BlockScoutWeb.API.V2.ForwardTransferView do
   alias Explorer.Chain.Address
 
   def render("forward_transfers.json", %{forward_transfers: forward_transfers, next_page_params: next_page_params}) do
-    %{"items" => Enum.map(forward_transfers, &prepare_forward_transfers(&1, nil)), "next_page_params" => next_page_params}
+    %{"items" => Enum.map(forward_transfers, &prepare_forward_transfer(&1, nil)), "next_page_params" => next_page_params}
   end
 
   def prepare_forward_transfer(forward_transfer, _conn) do
