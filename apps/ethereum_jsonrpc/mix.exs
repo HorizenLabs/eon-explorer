0d61438953b84a52f66ee1367d7f63b74c71a056
davide <davidebanfi@horizenlabs.io>
2024-05-15 15:37:21 +0200

    project update to version 3.6.0-RC1

--- apps/ethereum_jsonrpc/mix.exs
+++ apps/ethereum_jsonrpc/mix.exs
@@ -3,7 +3,7 @@ defmodule EthereumJsonrpc.MixProject do
 
   def project do
     [
-      version: "3.5.0",
+      version: "3.6.0-RC1",
       aliases: aliases(Mix.env()),
       app: :ethereum_jsonrpc,
       build_path: "../../_build",
--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule BlockScout.Mixfile do
     [
       # app: :block_scout,
       # aliases: aliases(config_env()),
-      version: "3.5.0",
+      version: "3.6.0-RC1",
       apps_path: "apps",
       deps: deps(),
       dialyzer: dialyzer(),
