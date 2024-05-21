9d6fafcde5a99d4a618fb080da1e30d7ead06b5f
davide <davidebanfi@horizenlabs.io>
2024-05-21 16:09:07 +0200

    project update to version 3.6.0-RC2

--- apps/ethereum_jsonrpc/mix.exs
+++ apps/ethereum_jsonrpc/mix.exs
@@ -3,7 +3,7 @@ defmodule EthereumJsonrpc.MixProject do
 
   def project do
     [
-      version: "3.6.0-RC1",
+      version: "3.6.0-RC2",
       aliases: aliases(Mix.env()),
       app: :ethereum_jsonrpc,
       build_path: "../../_build",
